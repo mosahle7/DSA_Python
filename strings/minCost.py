cda8f79e38fd52ce7696f52504d0839afc434815
mosahle7 <103498044+mosahle7@users.noreply.github.com>
2024-05-31 00:06:39 +0530

    Minimum cost to make two strings equal

--- strings/minCost.py
+++ strings/minCost.py
@@ -11,4 +11,20 @@ class Solution:
                 dp[j] = (dp[j] + dp[j - arr[i]]) % mod
 
         return dp[sum]%mod
+
+
+class Solution:
+	def findMinCost(self, x, y, costX, costY):
+		n,m = len(x),len(y)
+		dp=[[0]*(m+1) for i in range(n+1)]
+		
+		for i in range(1,n+1):
+			for j in range(1,m+1):
+				if x[i-1]==y[j-1]:
+					dp[i][j]=dp[i-1][j-1]+1
+				else: 
+					dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+		leng=dp[n][m]
+		tcost = (n-leng)*costX + (m-leng)*costY
+		return tcost
             
