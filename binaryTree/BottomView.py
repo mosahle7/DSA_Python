74ee8af5e9703681190bc2db18a32c6bdacb3e4b
mosahle7 <103498044+mosahle7@users.noreply.github.com>
2024-08-05 23:40:19 +0530

    Return a list of bottom eles having different horizontal distance from root

--- binaryTree/BottomView.py
+++ binaryTree/BottomView.py
@@ -1,3 +1,32 @@
+from collections import deque, defaultdict
+
+class Solution:
+    def bottomView(self, root):
+        if not root:
+            return []
+        
+        q = deque([(root, 0)])
+        d = defaultdict(int)
+        min_hd = max_hd = 0
+        
+        while q:
+            node, hd = q.popleft()
+            d[hd] = node.data
+            
+            min_hd = min(min_hd, hd)
+            max_hd = max(max_hd, hd)
+            
+            if node.left:
+                q.append((node.left, hd - 1))
+            if node.right:
+                q.append((node.right, hd + 1))
+        
+        # Create the result using the range of horizontal distances
+        ans = [d[hd] for hd in range(min_hd, max_hd + 1)]
+        return ans
+            
+
+
 class Solution:
     def bottomView(self, root):
         if not root:
