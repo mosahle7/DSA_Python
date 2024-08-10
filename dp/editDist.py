3e03179cfebd8785b3bf408d2bfb9c5219d2ebd7
mosahle7 <103498044+mosahle7@users.noreply.github.com>
2024-08-10 11:39:26 +0530

    mi

--- dp/editDist.py
+++ dp/editDist.py
@@ -38,7 +38,7 @@ class Solution:
             dp[0][i]=i
             
         for i in range(1,m+1):
-            for j in range(n+1):
+            for j in range(1,n+1):
                 if s1[i-1]==s2[j-1]:
                     dp[i][j]=dp[i-1][j-1]
                 
