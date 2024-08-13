5714702476dffdece520fe7c58ae0c579ff2b66e
mosahle7 <103498044+mosahle7@users.noreply.github.com>
2024-08-13 16:08:59 +0530

    Length of longest Increasing Subsequence

--- dp/longIncSubseq.py
+++ dp/longIncSubseq.py
@@ -11,3 +11,69 @@ class Solution:
         return max(dp)
        
 
+#User function Template for python3
+
+
+class Solution:
+    
+    #Function to find length of longest increasing subsequence.
+    def longestSubsequence(self, n, a):
+        def binSearch(s,ele):
+
+            l,r=0,len(s)-1
+            
+            while l<r:
+                mid=(l+r)//2
+                
+                if s[mid]>=ele:
+                    r=mid
+                else:
+                    l=mid+1
+            return l
+        
+        s=[]
+        s.append(a[0])
+        for i in range(1,n):
+            if a[i]>s[-1]:
+                s.append(a[i])
+              
+            else:
+                ind=binSearch(s,a[i])
+                s[ind]=a[i]
+               
+        return len(s)
+
+
+#User function Template for python3
+
+
+class Solution:
+    
+    #Function to find length of longest increasing subsequence.
+    def longestSubsequence(self, n, a):
+        def binSearch(s,ele):
+
+            l,r=0,len(s)-1
+            
+            while l<r:
+                mid=(l+r)//2
+                
+                if s[mid]>=ele:
+                    r=mid
+                else:
+                    l=mid+1
+            return l
+        
+        s=[]
+        s.append(a[0])
+        for i in range(1,n):
+            if a[i]>s[-1]:
+                s.append(a[i])
+              
+            else:
+                ind=binSearch(s,a[i])
+                s[ind]=a[i]
+               
+        return len(s)
+
+
