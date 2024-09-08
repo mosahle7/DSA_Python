9592d0d57c4ca09dd04b5ef936157a40125deb7e
mosahle7 <103498044+mosahle7@users.noreply.github.com>
2024-09-09 01:18:25 +0530

    Minimum number of jumps to reach end of array such that each element represents the max length of jump that can be made

--- arrays/minJumps.py
+++ arrays/minJumps.py
@@ -5,11 +5,14 @@ class Solution:
 	    lastInd=0
 	    jumps=0
 	    
-	    for i in range(0,n):
+	    if arr[0]==0 and n!=1: return -1
+	    
+	    for i in range(n):
 	        maxReach=max(maxReach,i+arr[i])
 	        if i==lastInd:
 	            if lastInd==maxReach: return -1
 	            lastInd=maxReach
 	            jumps+=1
 	            if lastInd>=n-1: return jumps
-	            
+	    return -1
+	    
