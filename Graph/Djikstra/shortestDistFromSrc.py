62596e50e473a2f841e9200767ad74a1c9c28c6b
mosahle7 <103498044+mosahle7@users.noreply.github.com>
2024-10-22 01:22:52 +0530

    Shortest Distance from source to all nodes

--- Graph/Djikstra/shortestDistFromSrc.py
+++ Graph/Djikstra/shortestDistFromSrc.py
@@ -16,4 +16,27 @@ class Solution:
                     heapq.heappush(h,[k[1]+dist,k[0]])
                     dis[k[0]]=k[1]+dist
         return dis
+
+from sortedcontainers import SortedSet
+
+class Solution:
+    def dijkstra(self, adj: List[List[Tuple[int, int]]], src: int) -> List[int]:
+        n=len(adj)
+        dis=[float('inf') for _ in range(n)]
+        s=SortedSet()
+        
+        s.add((0,src))
+        dis[src]=0
+
+        while s:
+            dist,node=s.pop(0)
+            for k in adj[node]:
+                
+                if dis[k[0]]>k[1]+dist:
+                    if dis[k[0]]!=float('inf'):
+                        s.remove((dis[k[0]],k[0]))
+                        
+                    s.add((k[1]+dist,k[0]))
+                    dis[k[0]]=k[1]+dist
+        return dis
         
