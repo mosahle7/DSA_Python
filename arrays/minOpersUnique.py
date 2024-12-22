ac0b11c34e3cc5173dca0326fd1ab5bd4d096dd8
mosahle7 <103498044+mosahle7@users.noreply.github.com>
2024-12-22 17:32:42 +0530

    Minimum number of operations required for unique elements
    Operations: Remove first 3 elements
    https://leetcode.com/problems/minimum-number-of-operations-to-make-elements-in-array-distinct/submissions/1485410378/

--- arrays/minOpersUnique.py
+++ arrays/minOpersUnique.py
@@ -1,4 +1,17 @@
-                
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        n=len(nums)
+        s=set()
+        for i in range(n-1,-1,-1):
+            if nums[i] not in s:
+                s.add(nums[i])
+            else:
+                ans=(i+1)//3
+                if (i+1)%3!=0:
+                    ans+=1
+                return ans
+        return 0
+      
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         ans=0
